4c80cbf758a745d350d3f0d4a10a5445f1933ee7
Diogo Anselmo <133712465+diogoanselmodasilva@users.noreply.github.com>
2024-09-17 20:13:16 -0300

    modificação para o ffslush e __fpurge
    Para fucionar em linux e windows, estou em duvida pq a função perguntas_facil não está sendo chamada, preciso fazer de preferencia um switch case pra facil, medio e dificil.

--- main.c
+++ main.c
@@ -3,35 +3,60 @@
 #include <stdlib.h>
 #include <locale.h>//para usar setlocale
 
-void main()
-{
-     // Configura o locale para permitir acentuação
-    setlocale(LC_ALL, "Portuguese");
 
-    // Criando as variáveis
-    char enunciado1[2][256] = {
-        "Qual o maior planeta do sistema solar?",
-        "Quantos planetas tem o sistema solar?"
-    };//O enunciado das perguntas
-    char alternativas1[2][4][256] = {
-        {"Terra", "Marte", "Jupiter", "Lua"},
-        {"3", "8", "9", "6"}
-    };//as alternativas das perguntas em ordem
-    char resp_correta1[] = {'c', 'b'};//contem a alternativa correta de cada pergunta facil
-    //perguntas de dificuldade media
-    char enunciado2[2][256] = {
-        "O que afirma a lei de Coloumb?",
-        "Qual é a unidade de medida de força no sistema internacional?"
-    };
-    //alternativas de dificuldade media
-    char alternativas2[2][4][256] = {
-        {" A força de atrito é diretamente proporcional à força normal", " A força elétrica entre duas cargas é inversamente proporcional ao quadrado da distância entre elas"," A energia cinética é a mesma que a energia potencial"," A velocidade de um objeto é constante se a força resultante for zero"},
-        {"Joule","Newton","Pascal","Watt"}
-    };
-    int i, j;
-    char resp;
-    char resp_correta2[] = {'b','b'};//resposta correta das dificuldades nivel medio
-    int dificuldade;
+// Criando as variáveis
+char enunciado1[2][256] = {
+    "Qual o maior planeta do sistema solar?",
+    "Quantos planetas tem o sistema solar?"
+};//O enunciado das perguntas
+char alternativas1[2][4][256] = {
+    {"Terra", "Marte", "Jupiter", "Lua"},
+    {"3", "8", "9", "6"}
+};//as alternativas das perguntas em ordem
+char resp_correta1[] = {'c', 'b'};//contem a alternativa correta de cada pergunta facil
+//perguntas de dificuldade media
+char enunciado2[2][256] = {
+    "O que afirma a lei de Coloumb?",
+    "Qual é a unidade de medida de força no sistema internacional?"
+};
+//alternativas de dificuldade media
+char alternativas2[2][4][256] = {
+    {" A força de atrito é diretamente proporcional à força normal", " A força elétrica entre duas cargas é inversamente proporcional ao quadrado da distância entre elas"," A energia cinética é a mesma que a energia potencial"," A velocidade de um objeto é constante se a força resultante for zero"},
+    {"Joule","Newton","Pascal","Watt"}
+};
+int i, j;
+char resp;
+char resp_correta2[] = {'b','b'};//resposta correta das dificuldades nivel medio
+int dificuldade;
+
+//função para perguntas faceis
+char perguntas_facil(){
+      for (i = 0; i < 2; i++) {
+        printf("\nPergunta %d: %s\n", i + 1, enunciado1[i]);//imprimi o enunciado das perguntas
+            for (j = 0; j < 4; j++) {
+                printf(" %c) %s\n", 'a' + j, alternativas1[i][j]);//imprimi as alternativas das perguntas
+            }
+            resp = getchar();
+            #ifdef _WIN32
+                system("ffslush(stdin");//para windows
+            Windows
+            #else
+                __fpurge(stdin);//para linux e macos
+             #endif
+            //verifica a resposta do jogador
+            if(resp == resp_correta1[i]){
+                printf("Certa resposta!");
+                }
+                else{
+                    printf("Resposta errada!");
+            }
+    }
+}
+//variavel pra receber a função
+
+void main(){
+    // Configura o locale para permitir acentuação
+    setlocale(LC_ALL, "Portuguese");
 
     printf("\n-----Bem-vindo ao jogo-----\n");
     printf("\n-----Pressione enter para continuar -----\n");
@@ -48,26 +73,10 @@ void main()
     printf("\n1- Fácil\n");
     printf("\n2- Médio\n");
     dificuldade = getchar();
-    if(dificuldade == 1){
-        for (i = 0; i < 2; i++) {
-        printf("\nPergunta %d: %s\n", i + 1, enunciado1[i]);//imprimi o enunciado das perguntas
-            for (j = 0; j < 4; j++) {
-                printf(" %c) %s\n", 'a' + j, alternativas1[i][j]);//imprimi as alternativas das perguntas
-            }
-            resp = getchar();
-            __fpurge(stdin);
-            //verifica a resposta do jogador
-            if(resp == resp_correta1[i]){
-                printf("Certa resposta!");
-                }
-                else{
-                    printf("Resposta errada!");
-            }
-
-        }
+    __fpurge(stdin);
+    switch(dificuldade){
+        case 1: perguntas_facil();
+        break;
     }
-
-
-
-
 }
+
