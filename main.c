7f531221028f770eec9298b79301ad91e730a597
Diogo Anselmo <133712465+diogoanselmodasilva@users.noreply.github.com>
2024-09-18 21:17:20 -0300

    adição de while, e pontuacao()
    toda vez que o jogador terminar uma rodada perguntar se ele gostaria de jogar de novo

--- main.c
+++ main.c
@@ -24,19 +24,32 @@ char enunciado3[2][256] = {
     "Qual é a principal razão pela qual Plutão não é mais considerado um planeta do Sistema Solar?",
     "O que afirma o princípio da incerteza de Heisenberg?"
 };
-char alternativas3[2][4][256] = {
-    {"Sua órbita é inclinada em relação ao plano do Sistema Solar.", "Não é grande o suficiente para limpar sua órbita de outros objetos.", "Ele não possui satélites naturais.", "Sua composição é predominantemente gasosa."}
-    {"Não é possível medir simultaneamente a posição e a velocidade de uma partícula com precisão arbitrária.","A energia total de um sistema isolado é sempre constante.","A luz pode se comportar como partícula e como onda.","A gravidade é a força mais fraca entre as quatro forças fundamentais."}
-};
+
 //alternativas de dificuldade media
 char alternativas2[2][4][256] = {
     {" A força de atrito é diretamente proporcional à força normal", " A força elétrica entre duas cargas é inversamente proporcional ao quadrado da distância entre elas"," A energia cinética é a mesma que a energia potencial"," A velocidade de um objeto é constante se a força resultante for zero"},
     {"Joule","Newton","Pascal","Watt"}
 };
+char alternativas3[2][4][256] = {
+    {
+        "Sua órbita é inclinada em relação ao plano do Sistema Solar.",
+        "Não é grande o suficiente para limpar sua órbita de outros objetos.",
+        "Ele não possui satélites naturais.",
+        "Sua composição é predominantemente gasosa."
+    },
+    {
+        "Não é possível medir simultaneamente a posição e a velocidade de uma partícula com precisão arbitrária.",
+        "A energia total de um sistema isolado é sempre constante.",
+        "A luz pode se comportar como partícula e como onda.",
+        "A gravidade é a força mais fraca entre as quatro forças fundamentais."
+    }
+};
+char resp_correta3[] = {'b','a'};
 int i, j;
 char resp;
 char resp_correta2[] = {'b','b'};//resposta correta das dificuldades nivel medio
-int dificuldade;
+int dificuldade, point;
+char next;
 //função para perguntas faceis
 char perguntas_facil(){
     for (i = 0; i < 2; i++) {
@@ -77,7 +90,7 @@ char perguntas_medio(){
                 __fpurge(stdin);//para linux e macos
              #endif
             //verifica a resposta do jogador
-            if(resp == resp_correta1[i]){
+            if(resp == resp_correta2[i]){
                 printf("Certa resposta!");
                 }
                 else{
@@ -85,6 +98,48 @@ char perguntas_medio(){
             }
     }
 }
+char perguntas_dificil(){
+     for (i = 0; i < 2; i++) {
+        printf("\nPergunta %d: %s\n", i + 1, enunciado3[i]);//imprimi o enunciado das perguntas
+            for (j = 0; j < 4; j++) {
+                printf(" %c) %s\n", 'a' + j, alternativas3[i][j]);//imprimi as alternativas das perguntas
+            }
+            resp = getchar();
+            #ifdef _WIN32
+                system("ffslush(stdin");//para windows
+            Windows
+            #else
+                __fpurge(stdin);//para linux e macos
+             #endif
+            //verifica a resposta do jogador
+            if(resp == resp_correta3[i]){
+                printf("Certa resposta!");
+                }
+                else{
+                    printf("Resposta errada!");
+            }
+    }
+}
+int pontucao(){
+    point = 0;
+    switch(dificuldade){
+      case 1:
+            if(resp == resp_correta1[i]){
+                point++;
+            } break;
+        case 2:
+            if(resp==resp_correta2[i]){
+                point++;
+            }
+        break;
+        case 3:
+            if(resp == resp_correta3[i]){
+                point++;
+            }
+        break;
+    };
+    return point;
+};
 
 void main(){
     // Configura o locale para permitir acentuação
@@ -107,19 +162,35 @@ void main(){
     #else
         system("clear");//comano para Linux e MacOs
     #endif
+    do{
+        printf("Escolha a dificuldade:");
+        printf("\n1- Fácil\n");
+        printf("\n2- Médio\n");
+        printf("\n3-Dificil\n");
+        scanf("%d",&dificuldade);
+        __fpurge(stdin);
 
-    printf("Escolha a dificuldade:");
-    printf("\n1- Fácil\n");
-    printf("\n2- Médio\n");
-    scanf("%d",&dificuldade);
-    __fpurge(stdin);
-
-    switch(dificuldade){
-        case 1: perguntas_facil();
-        break;
-        case 2: perguntas_medio();
-        break;
-        default: printf("Por favor escolha um nível de dificuldade!");
-        }
+        switch(dificuldade){
+            case 1: perguntas_facil();
+            break;
+            case 2: perguntas_medio();
+            break;
+            case 3: perguntas_dificil();
+            break;
+            default: printf("Por favor escolha um nível de dificuldade!");
+            }
+            printf("\nParabens, sua pontuação é %d\n", point);
+            getchar();
+             #ifdef _WIN32
+            system("ffslush(stdin");//para windows
+        Windows
+        #else
+            __fpurge(stdin);//para linux e macos
+        #endif
+        //verifica a resposta do jogador
+            printf("\nDeseja continuar?\n");
+            printf("\nS-sim\n");
+            printf("\nN-não\n");
+            scanf("%s",&next);
+    }while (next == 's');
 }
-
